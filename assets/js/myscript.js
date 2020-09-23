b6ff92ddacec446397667783ef50aba3a1f51cc4
Yaco-99 <coopman.yannick@gmail.com>
2020-09-23 16:58:04 +0200

    map

--- assets/js/myscript.js
+++ assets/js/myscript.js
@@ -36,22 +36,17 @@ function restaurantChoice(){
     let restaurantImg = document.getElementById("restaurant-img");
     let restaurantLink = document.getElementById("restaurantLink");
     restaurant = parseInt(restaurant);
-    restaurantImg.src = "#";
-    adress.innerHTML = "";
-    restaurantLink.href = "";
+    restaurantImg.src = "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2542.9270521105796!2d4.437695215729751!3d50.40519707946908!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47c227637e52f8cd%3A0xc86e426105b5a0e5!2sE6K%20-%20Square%20des%20Martyrs%201%2C%206000%20Charleroi!5e0!3m2!1sfr!2sbe!4v1600872311903!5m2!1sfr!2sbe";
+    adress.innerHTML = "KELLER - Square des Martyrs 1, 6000 Charleroi";
     switch (restaurant){
         case 0:
-            restaurantImg.src = "assets/img/where.jpg";
-            break;
+          adress.innerHTML = "KELLER - Square des Martyrs 1, 6000 Charleroi";
+          restaurantImg.src = "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2542.9270521105796!2d4.437695215729751!3d50.40519707946908!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47c227637e52f8cd%3A0xc86e426105b5a0e5!2sE6K%20-%20Square%20des%20Martyrs%201%2C%206000%20Charleroi!5e0!3m2!1sfr!2sbe!4v1600872311903!5m2!1sfr!2sbe";
+          break;
         case 1:
-            adress.innerHTML = "KELLER - Square des Martyrs 1, 6000 Charleroi";
-            restaurantImg.src = "assets/img/Charleroi.jpg";
-            restaurantLink.href ="https://www.google.com/maps/place/E6K+-+Square+des+Martyrs+1,+6000+Charleroi/@50.4052005,4.4376899,17z/data=!3m1!4b1!4m5!3m4!1s0x47c227637e52f8cd:0xc86e426105b5a0e5!8m2!3d50.4051971!4d4.4398839";
-            break;
-        case 2:
-            adress.innerHTML = " Cantersteen 10, 1000 Bruxelles";
-            restaurantImg.src = "assets/img/Bruxelles.jpg";
-            restaurantLink.href = "https://www.google.com/maps?q=Cantersteen+10,+1000+Bruxelles&um=1&ie=UTF-8&sa=X&ved=2ahUKEwii8aaFjvrrAhVB_KQKHaSJCK0Q_AUoAXoECBAQAw"
-            break;
+          adress.innerHTML = "Cantersteen 10, 1000 Bruxelles";
+          restaurantImg.src = src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2519.2335829397703!2d4.355091250886594!3d50.84535866661197!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47c3c47fce0ba71d%3A0x2f634768a374d3b0!2sCantersteen%2010%2C%201000%20Bruxelles!5e0!3m2!1sfr!2sbe!4v1600872954627!5m2!1sfr!2sbe";
+        
+          break; 
     }
 }
